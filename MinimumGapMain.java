8a70a57b63fe53c4839f79c5ba1c3dc57fc47d30
May Chi <116498227+maychilo@users.noreply.github.com>
2022-11-11 11:29:54 +0700

    Update MinimumGapMain.java

--- MinimumGapMain.java
+++ MinimumGapMain.java
@@ -9,16 +9,15 @@ public class MinimumGapMain {
 		public static void main(String[] args) {
 			List<Integer> arr  = new ArrayList<Integer>(asList(7,1,3,4,1,7));
 			int mingap = minimumGap(arr);
-			System.out.println("gap " + mingap);
+			System.out.println("mininum gap " + mingap);
 		}
 
 		public static int minimumGap(List<Integer> arr) {
 			int mingap = arr.size() - 1;
 			int gap = 0;
 			for (int i = 0; i < arr.size(); i++) {
-				int tempNum = arr.get(i);
 				for (int j = 0; j < arr.size(); j++) {
-					if (tempNum == arr.get(j) & i != j) {
+					if (arr.get(i)== arr.get(j) & i != j) {
 						gap = Math.abs(j - i);
 					}
 					if (mingap > gap & gap != 0) {
